eb1f520491ec7aa05fb995c83768dc7bb1850709
Takuya Sudo <t-sudo@goem.co.jp>
2024-03-02 18:23:04 +0900

    プロジェクト作成

---
+++ location_accurary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  location_accurary
+//
+//  Created by takuyasudo on 2024/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
