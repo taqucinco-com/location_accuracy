c08d596fa30e6300e61ff47523db15446e337b63
Takuya Sudo <someone.said.so@gmail.com>
2024-03-03 15:07:04 +0900

    swiftuiのプロジェクトを作成

---
+++ location_accuracy/location_accuracyApp.swift
@@ -0,0 +1,17 @@
+//
+//  location_accuracyApp.swift
+//  location_accuracy
+//
+//  Created by takuyasudo on 2024/03/03.
+//
+
+import SwiftUI
+
+@main
+struct location_accuracyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ location_accuracyUITests/location_accuracyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  location_accuracyUITestsLaunchTests.swift
+//  location_accuracyUITests
+//
+//  Created by takuyasudo on 2024/03/03.
+//
+
+import XCTest
+
+final class location_accuracyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
